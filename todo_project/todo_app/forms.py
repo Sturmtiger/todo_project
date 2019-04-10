6c9e5512a3810cb6cb982fb1dfccb3071a8ffb18
Maxim Shiryaev <maksim155555@gmail.com>
2019-04-10 05:47:15 +0300

    final

--- todo_project/todo_app/forms.py
+++ todo_project/todo_app/forms.py
@@ -2,11 +2,14 @@ from django import forms
 from .models import *
 from datetime import datetime as dt
 from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
 
 class ProjectForm(forms.ModelForm):
-    # def __init__(self, user=None, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.user = user
+    def __init__(self, user=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
 
     class Meta:
         model = Project
@@ -16,11 +19,17 @@ class ProjectForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'project name', 'autofocus': 'autofocus'})
         }
 
-    # def clean(self):
-    #     new_project = self.cleaned_data['name']
-    #     if Project.objects.filter(user=self.user, name__iexact=new_project).count():
-    #         raise ValidationError('Project name must be unique!')
-    #     return new_project
+    def clean_name(self):
+        new_project = self.cleaned_data['name']
+        if Project.objects.filter(user=self.user, name__iexact=new_project).count():
+            raise ValidationError('Project name must be unique!')
+        return new_project
+
+    def clean_colour(self):
+        new_colour = self.cleaned_data['colour']
+        if Project.objects.filter(user=self.user, colour=new_colour).count():
+            raise ValidationError('Project colour must be unique!')
+        return new_colour
 
 
 class TaskForm(forms.ModelForm):
@@ -28,10 +37,10 @@ class TaskForm(forms.ModelForm):
         widget=forms.DateTimeInput(attrs={
             'class': 'form-control',
             'type': 'datetime-local',
-            'min': dt.now().strftime('%Y-%m-%dT%H:%M'),  # realize jQuery
-            'max': dt.now().replace(year=dt.now().year+2).strftime('%Y-%m-%dT%H:%M')  # realize jQuery
+            'min': dt.now().strftime('%Y-%m-%dT00:00'),
+            'max': dt.now().replace(year=dt.now().year+2, day=dt.now().day+1).strftime('%Y-%m-%dT00:00')  # until 2 years
         }),
-        initial=dt.now().strftime('%Y-%m-%dT%H:%M')
+        initial=dt.now().strftime('%Y-%m-%dT00:00')
     )
 
     def __init__(self, user=None, *args, **kwargs):
@@ -53,6 +62,11 @@ class TaskForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'task description', 'autofocus': 'autofocus'}),
             'priority': forms.Select(attrs={'class': 'form-control'}),
-            # 'date_until': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
-            # 'project': forms.Select(attrs={'class': 'form-control'}),
         }
+
+    def clean_date_until(self):
+        new_date_until = self.cleaned_data['date_until']
+        print(new_date_until < timezone.now())
+        if new_date_until < timezone.now() or new_date_until > timezone.now().replace(year=timezone.now().year+2):
+            raise ValidationError('Datetime values error. Notice: 2 years is maximum!')
+        return new_date_until
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -6,82 +6,96 @@ from .forms import *
 from django.shortcuts import redirect
 from django.views.generic import View
 from .utils import *
-# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+from django.template.defaultfilters import register
 # Create your views here.
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+
 
 def main(request):
     return redirect('today_url')
 
-
+@login_required
 def today(request):
-    if request.user.is_authenticated:
-        # today_date = datetime.today().strftime("%a, %d %B")
-        project_form = ProjectForm()
-        task_form = TaskForm(request.user)
-        projects = Project.objects.filter(user=request.user)
-        overdue_tasks = Task.objects.filter(project__user=request.user,
-                                            date_until__lt=timezone.now(),
-                                            status='')
-        tasks = Task.objects.filter(project__user=request.user, #  for a day
-                                    date_until__gte=timezone.now(),
-                                    date_until__lt=timezone.now()+timezone.timedelta(days=1))
+    project_form = ProjectForm()
+    task_form = TaskForm(request.user)
+    projects = Project.objects.filter(user=request.user)
+    overdue_tasks = Task.objects.filter(project__user=request.user,
+                                        date_until__lt=timezone.now(),
+                                        status='')
+    tasks = Task.objects.filter(project__user=request.user, #  for a day
+                                date_until__gte=timezone.now(),
+                                date_until__lt=timezone.now()+timezone.timedelta(days=1),
+                                status='')
+    tasks_counted = {project.name: project.task_set.filter(date_until__gte=timezone.now(),
+                                                           date_until__lt=timezone.now()+timezone.timedelta(days=1),
+                                                           status='').count() for project in projects}
 
-        context = {
-            'today_obj': True,
-            'title': 'Today',
-            'today_date': datetime.now(),
-            'projects': projects,
-            'overdue_tasks': overdue_tasks,
-            'tasks': tasks,
-            'project_form': project_form,
-            'task_form': task_form,
-        }
-        return render(request, 'todo_app/main.html', context=context)
+    context = {
+        'today_obj': True,
+        'title': 'Today',
+        'today_date': datetime.now(),
+        'projects': projects,
+        'overdue_tasks': overdue_tasks,
+        'tasks': tasks,
+        'tasks_counted': tasks_counted,
+        'project_form': project_form,
+        'task_form': task_form,
+    }
+    return render(request, 'todo_app/main.html', context=context)
 
-    return redirect('login_url')
 
-
+@login_required
 def nextSevenDays(request):
-    if request.user.is_authenticated:
-        # today_date = datetime.today().strftime("%a, %d %B")
-        project_form = ProjectForm()
-        task_form = TaskForm(user=request.user)
-        projects = Project.objects.filter(user=request.user)
-        tasks = Task.objects.filter(project__user=request.user, #  for next 7 days
-                                    date_until__gte=timezone.now()+timezone.timedelta(days=1),
-                                    date_until__lt=timezone.now()+timezone.timedelta(days=8))
+    project_form = ProjectForm()
+    task_form = TaskForm(user=request.user)
+    projects = Project.objects.filter(user=request.user)
+    tasks = Task.objects.filter(project__user=request.user,
+                                date_until__gte=timezone.now()+timezone.timedelta(days=1),
+                                date_until__lt=timezone.now()+timezone.timedelta(days=8),
+                                status='')
+    tasks_counted = {project.name: project.task_set.filter(date_until__gte=timezone.now()+timezone.timedelta(days=1),
+                                                           date_until__lt=timezone.now()+timezone.timedelta(days=8),
+                                                           status='').count() for project in projects}
+    context = {
+        'next7days_obj': True,
+        'title': 'Next 7 days',
+        'today_date': datetime.now(),
+        'projects': projects,
+        'tasks': tasks,
+        'tasks_counted': tasks_counted,
+        'project_form': project_form,
+        'task_form': task_form,
+    }
+    return render(request, 'todo_app/main.html', context=context)
 
-        context = {
-            'next7days_obj': True,
-            'title': 'Next 7 days',
-            'today_date': datetime.now(),
-            'projects': projects,
-            'tasks': tasks,
-            'project_form': project_form,
-            'task_form': task_form,
-        }
-        return render(request, 'todo_app/main.html', context=context)
-
-    return redirect('login_url')
 
 
+@login_required
 def projectDetail(request, slug):
     user = User.objects.get(id=request.user.id)
+    projects = user.project_set.all()
     project = user.project_set.get(slug=slug)
     tasks = project.task_set.all()
     context = {
+        'slug': slug,
         'title': project.name + ' PROJECT',
+        'projects': projects,
         'project': project,
         'tasks': tasks}
     return render(request, 'todo_app/project.html', context=context)
 
 @login_required
 def archive(request):
-    user = User.objects.get(id=request.user.id)
-    projects = user.project_set.all()
-    tasks = [task for project in projects for task in project.task_set.filter(status='done')]
+    tasks = Task.objects.filter(project__user=request.user,
+                                status='done')
     context = {
         'tasks': tasks,
         'title': 'Archive'
@@ -89,12 +103,10 @@ def archive(request):
     return render(request, 'todo_app/archive.html', context=context)
 
 
-class ProjectCreate(View):
+class ProjectCreate(LoginRequiredMixin, View):
     def post(self, request):
-        bound_form = ProjectForm(request.POST)
-        print(request.POST)
-        print(bound_form.is_valid())
-        print(bound_form.errors)
+        bound_form = ProjectForm(user=request.user, data=request.POST)
+
         if bound_form.is_valid():
             new_project = bound_form.save(commit=False)
             new_project.user = User.objects.get(id=request.user.id)
@@ -104,7 +116,7 @@ class ProjectCreate(View):
         return redirect('main_url', permanent=True)
 
 
-class TaskCreate(View):
+class TaskCreate(LoginRequiredMixin, View):
     def post(self, request):
         post = request.POST.copy()
         post['date_until'] = post.get('date_until').replace('T', ' ')
@@ -115,9 +127,8 @@ class TaskCreate(View):
 
         return redirect('main_url', permanent=True)
 
-
+@login_required
 def projectDelete(request, slug):
-    print(request.user)
     if request.method == 'POST':
         user = User.objects.get(id=request.user.id)
         project = user.project_set.get(slug=slug)
@@ -131,24 +142,25 @@ def projectDelete(request, slug):
         messages.success(request, f'Project "{project.name}" has been deleted successfully!')
         return redirect('main_url', permanent=True)
 
-
+@login_required
 def taskDelete(request, id):
     if request.method == 'POST':
         task = Task.objects.get(id=id)
         task.delete()
-        messages.success(request, f'Task <b>"{task.name}"</b> has been deleted successfully!')
+        messages.success(request, f'Task "{task.name}" has been deleted successfully!')
         return redirect('main_url', permanent=True)
 
-
+@login_required
 def taskDone(request, id):
     if request.method == 'POST':
         task = Task.objects.get(id=id)
         task.status = 'done'
         task.save()
+        messages.success(request, f'Task "{task.name}" done! Well done!:)')
         return redirect('main_url', permanent=True)
 
 
-class projectUpdate(View):
+class projectUpdate(LoginRequiredMixin, View):
     def get(self, request, slug):
         project = Project.objects.get(slug=slug)
         bound_form = ProjectForm(instance=project)
@@ -160,7 +172,7 @@ class projectUpdate(View):
 
     def post(self, request, slug):
         project = Project.objects.get(slug=slug)
-        bound_form = ProjectForm(request.user, post, instance=project)
+        bound_form = ProjectForm(request.user, request.POST, instance=project)
         if bound_form.is_valid():
             bound_form.save()
             return redirect('main_url')
@@ -170,7 +182,8 @@ class projectUpdate(View):
         }
         return render(request, 'todo_app/update_universal.html', context=context)
 
-class taskUpdate(View):
+
+class taskUpdate(LoginRequiredMixin, View):
     def get(self, request, id):
         task = Task.objects.get(id=id)
         bound_form = TaskForm(request.user, instance=task)
